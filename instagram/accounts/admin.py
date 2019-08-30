404d95abdc351b0cda678233ce05acf7a5a2ff0f
Aldik <obi1konobe@gmail.com>
2019-08-30 20:24:28 +0600

    в accounts создана модель Profile и зарегистрирована в админке

--- instagram/accounts/admin.py
+++ instagram/accounts/admin.py
@@ -1,3 +1,18 @@
-from django.contrib import admin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from instagram.accounts.models import Profile
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    fields = ['avatar', 'email', 'about_me', 'phone_number', 'gender']
+
+
+class ProfileAdmin(UserAdmin):
+    inlines = [ProfileInline]
+
+
+admin.site.unregister(User)
+admin.site.register(User, ProfileAdmin)
--- instagram/accounts/models.py
+++ instagram/accounts/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        User,
+        related_name='profile',
+        on_delete=models.CASCADE,
+        verbose_name='User'
+    )
+
+    email = models.EmailField(
+        blank=False,
+        null=False,
+        max_length=50,
+        verbose_name='Email'
+    )
+
+    avatar = models.ImageField(
+        null=False,
+        blank=False,
+        upload_to='user_pics',
+        verbose_name='Avatar'
+    )
+
+    about_me = models.TextField(
+        null=True,
+        blank=True,
+        max_length=1000,
+        verbose_name='About me'
+    )
+
+    phone_number = models.CharField(
+        max_length=12,
+        null=True,
+        blank=True,
+        verbose_name='Phone number'
+    )
+
+    gender = models.CharField(
+        null=True,
+        blank=True,
+        verbose_name='Gender',
+        max_length=10,
+    )
+
+    def __str__(self):
+        return self.user.get_full_name() + "'s Profile"
+
+    class Meta:
+        verbose_name = 'Profile'
+        verbose_name_plural = 'Profiles'
