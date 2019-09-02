33edc530371bdafcf93184573b5398c451c4a6a7
Aldik <obi1konobe@gmail.com>
2019-09-02 15:32:10 +0600

    создана модель Post

--- instagram/webapp/models.py
+++ instagram/webapp/models.py
@@ -1,3 +1,29 @@
+from django.conf import settings
+from django.db.models import ImageField
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    picture = ImageField(
+        null=True,
+        blank=True,
+        upload_to='user_pics',
+        verbose_name='Picture'
+    )
+
+    description = models.TextField(
+        max_length=200,
+        null=True,
+        blank=True,
+        verbose_name='Description',
+    )
+
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='pictures',
+        on_delete=models.PROTECT,
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Add time')
