027bbc46cf651793dc16ae6c7ac086ac880a1c39
Aldik <obi1konobe@gmail.com>
2019-09-03 12:48:09 +0600

    подключено представление UserDetailView

--- instagram/accounts/urls.py
+++ instagram/accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 
-from .views import register_view
+from .views import register_view, UserDetailView
 
 app_name = 'accounts'
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('login', LoginView.as_view(), name='login'),
     path('logout', LogoutView.as_view(), name='logout'),
     path('create/', register_view, name='create'),
+    path('<int:pk>', UserDetailView.as_view(), name='user_detail')
 ]
--- instagram/accounts/views.py
+++ instagram/accounts/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
+from django.views.generic import DetailView
 
 from accounts.forms import UserCreationForm
 
@@ -14,3 +16,9 @@ def register_view(request, *args, **kwargs):
     else:
         form = UserCreationForm()
     return render(request, 'create_user.html', context={'form': form})
+
+
+class UserDetailView(DetailView):
+    model = User
+    template_name = 'user_detail.html'
+    context_object_name = 'user_obj'
